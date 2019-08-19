f60db672f6c214d16748c6492c26c97fe48bc89b
renan <camposrenanrc@gmail.com>
2019-08-19 16:41:50 -0300

    adicionar atividade incompleta

---
+++ exercicio2.py
@@ -0,0 +1,15 @@
+numeros = int(input('Digite que tipo de numero vc deseja: '))
+
+function =
+
+impares = ['1', '3', '5', '7', '9']
+pares = ['2', '4', '6', '8', '10']
+
+if numeros == impares :
+    print('1', '3', '5', '7', '9')
+
+elif numeros == pares :
+    print('2', '4', '6', '8', '10')
+
+else:
+    print('Numero não encontrado')
